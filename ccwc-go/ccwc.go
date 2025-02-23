2b9d6c9f629d8a9b9fb810f6812b0224bdffa231
jmagaud <jmagaud@kali>
2025-02-23 11:25:36 -0500

    byte counter

--- ccwc-go/ccwc.go
+++ ccwc-go/ccwc.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func byteCount(filename string) int {
+	file, err := os.Open(filename)
+	check(err)
+	defer file.Close()
+	// get the file size
+	stat, err := file.Stat()
+	check(err)
+	size := stat.Size()
+
+}
 
 func main() {
 	fmt.Println("test")
+	counterFlag := flag.Bool("c", false, "count the number of characters")
+	wordFlag := flag.Bool("w", false, "count the number of words")
+	lineFlag := flag.Bool("l", false, "count the number of lines")
+	flag.Parse()
+	if !*counterFlag && !*wordFlag && !*lineFlag {
+		fmt.Println("Please specify at least one flag")
+		return
+	}
+	tail := flag.Args()
+	if len(tail) == 0 {
+		fmt.Println("Please specify a file")
+		return
+	}
+	filename := tail[0]
+
+	if *counterFlag {
+		byteCount(filename)
+	}
+
 }
